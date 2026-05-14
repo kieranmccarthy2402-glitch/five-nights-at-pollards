e3e558c7934329ff5fd07765a3f1f5ed50c47d76
kieranmccarthy2402-glitch <kieranmccarthy2402@gmail.com>
2026-05-15 10:41:23 +1200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,108 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "~;r%*V7!}EakIv*Q:qv(_img":
+            case "Ollie":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . e . e . . . . . . . 
+. . . . . . e e e e . . . . . . 
+. . . . . . d d d d . . . . . . 
+. . . . . . d f d f . . . . . . 
+. . . . . . d d d d . . . . . . 
+. . . . . e d d d d . . . . . . 
+. . . . 8 8 e 8 8 8 8 8 . . . . 
+. . . . 8 . 8 e 8 8 . 8 . . . . 
+. . . . 8 . 8 8 f e . 8 . . . . 
+. . . . 8 . 8 8 e f e 8 . . . . 
+. . . . 8 . 8 8 8 e f e . . . . 
+. . . . . . 8 . . 8 e . . . . . 
+. . . . . . 8 . . 8 . . . . . . 
+`;
+            case "image1":
+            case "Nate":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . e e e e . . . . . . . 
+. . . . . f d d f . . . . . . . 
+. . . . . d d d d . . . . . . . 
+. . . f . d d d d . . . . . . . 
+. . . 8 f 8 8 8 8 8 8 . . . . . 
+. . . 8 . f 8 8 8 . 8 . . . . . 
+. . . 8 . 8 f f 8 . 8 . . . . . 
+. . . 8 . 8 f 1 f . 8 . . . . . 
+. . . . . 8 8 f 1 f . . . . . . 
+. . . . . 8 . . f f . . . . . . 
+. . . . . 8 . . 8 . . . . . . . 
+. . . . . 8 . . 8 . . . . . . . 
+. . . . . 8 . . 8 . . . . . . . 
+. . . . . 8 . . 8 . . . . . . . 
+`;
+            case "image2":
+            case "Levi":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 4 4 4 . . . . . . . 
+. . . . . . f d f . . . . . . . 
+. . . . . . d d d . . . . . . . 
+. . . . . 2 8 8 8 2 . . . . . . 
+. . . . . 2 8 8 8 2 . . . . . . 
+. . . . . 2 8 8 8 2 . . . . . . 
+. . . . . 2 8 8 8 2 . . . . . . 
+. . . . . 2 8 8 8 2 . . . . . . 
+. . . . . . 8 . 8 . . . . . . . 
+. . . . . . 8 . 8 . . . . . . . 
+. . . . . . 8 . 8 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "dAuxz_j/KsG`4)z;t$(6_img":
+            case "Marcus":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f . . . . . . . 
+. . . . . d d d d . . . . . . . 
+. . . . . f d d f . . . . . . . 
+. . . . . d d d d . . . . . . . 
+. . . . . d d d d . . . . . . . 
+. . . . 8 8 2 2 8 8 . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . . . 8 . 8 . . . . . . . 
+. . . . . . 8 . 8 . . . . . . . 
+. . . . . . 8 . 8 . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
